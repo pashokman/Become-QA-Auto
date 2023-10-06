7151b5e36c12229d5092245327d86ae61c7ab4fe
Pavlo Lekhitskyi <pashokman1@gmail.com>
2023-10-06 13:38:09 +0300

    add fixture User; add 2 marks (check, change)

---
+++ tests/conftest.py
@@ -0,0 +1,24 @@
+import pytest
+
+class User:
+
+    def __init__(self):
+        self.name = None
+        self.second_name = None
+
+    def create(self):
+        self.name = 'Pavlo'
+        self.second_name = 'Lekhitskyi'
+
+    def remove(self):
+        self.name = ''
+        self.second_name = ''
+
+@pytest.fixture
+def user():
+    user = User()
+    user.create()
+
+    yield user
+    
+    user.remove()
