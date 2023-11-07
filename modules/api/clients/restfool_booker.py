c9ff61dc1403298845d5a61804809ceae7983974
Pavlo Lekhitskyi <pashokman1@gmail.com>
2023-11-07 15:45:02 +0200

    add get/update/delete methods tests into restfool_booking test file and module

--- modules/api/clients/restfool_booker.py
+++ modules/api/clients/restfool_booker.py
@@ -52,6 +52,49 @@ class Restbooker:
 
         return body
     
+    def get_booking(self, booking_id, status_code):
+        request = requests.get(f"{Restbooker.MAIN_URL}/booking/{booking_id}")
+
+        assert request.status_code == status_code
+        self.log.debug("Get booking successful.")
+        if status_code == 200:
+            body = request.json()
+            return body
+    
+    def update_booking(self, token, booking_id, firstname: str, lastname: str, totalprice: int, depositpaid: bool, 
+                            checkin: str, checkout: str, additionalneeds: str):
+        updated_booking = {
+            "firstname" : f"{firstname}",
+            "lastname" : f"{lastname}",
+            "totalprice" : totalprice,
+            "depositpaid" : depositpaid,
+            "bookingdates" : {
+                "checkin" : f"{checkin}",
+                "checkout" : f"{checkout}"
+            },
+            "additionalneeds" : f"{additionalneeds}"
+        }
+
+        request = requests.put(f"{Restbooker.MAIN_URL}/booking/{booking_id}", 
+                               json=updated_booking, 
+                               headers={"Cookie": f"token={token}"})
+
+        body = request.json()
+        assert request.status_code == 200
+        self.log.debug("Update booking successful.")
+
+        return body
+    
+
+    def delete_booking(self, token, request_body):
+        booking_id = request_body['bookingid']
+
+        request = requests.delete(f"{Restbooker.MAIN_URL}/booking/{booking_id}", 
+                               headers={"Cookie": f"token={token}"})
+
+        assert request.status_code == 201
+        self.log.debug("Delete booking successful.")
+    
 
         # Assertions
     def auth_assertion(self):
@@ -82,3 +125,41 @@ class Restbooker:
 
         assert request_body == expected_booking_object
         self.log.info("Created booking object is equal to expected booking object.")
+
+
+    def get_booking_assertion(self, firstname: str, lastname: str, totalprice: int, depositpaid: bool, 
+                            checkin: str, checkout: str, additionalneeds: str, request_body):
+        expected_booking_object = {
+            "firstname" : f"{firstname}",
+            "lastname" : f"{lastname}",
+            "totalprice" : totalprice,
+            "depositpaid" : depositpaid,
+            "bookingdates" : {
+                "checkin" : f"{checkin}",
+                "checkout" : f"{checkout}"
+            },
+            "additionalneeds" : f"{additionalneeds}"
+        }
+
+        assert request_body == expected_booking_object
+        self.log.info("Getted booking object is equal to expected booking object.")
+
+    def update_booking_assertion(self, firstname: str, lastname: str, totalprice: int, depositpaid: bool, 
+                            checkin: str, checkout: str, additionalneeds: str, request_body):
+        expected_booking_object = {
+            "firstname" : f"{firstname}",
+            "lastname" : f"{lastname}",
+            "totalprice" : totalprice,
+            "depositpaid" : depositpaid,
+            "bookingdates" : {
+                "checkin" : f"{checkin}",
+                "checkout" : f"{checkout}"
+            },
+            "additionalneeds" : f"{additionalneeds}"
+        }
+
+        assert request_body == expected_booking_object
+        self.log.info("Updated booking object is equal to expected booking object.")
+
+    def delete_booking_assertion(self, request_body, status_code):
+        self.get_booking(request_body["bookingid"], status_code)
--- tests/api/test_restfool_booker.py
+++ tests/api/test_restfool_booker.py
@@ -1,5 +1,6 @@
 import pytest
 
+
 @pytest.mark.restbooker
 def test_start(restbooker):
     restbooker.start()
@@ -11,10 +12,40 @@ def test_auth(restbooker):
 
 
 @pytest.mark.restbooker
-def test_user_creation(restbooker):
-    booking = restbooker.create_booking("Lester", "Tester", 12000, True, "2023-11-11", "2023-11-12", "two bathrooms")
+def test_booking_creation(restbooker):
+    new_booking = restbooker.create_booking("Lester", "Tester", 12000, True, "2023-11-11", "2023-11-12", "two bathrooms")
 
-    restbooker.create_booking_assertion("Lester", "Tester", 12000, True, "2023-11-11", "2023-11-12", "two bathrooms", booking)
+    restbooker.create_booking_assertion("Lester", "Tester", 12000, True, "2023-11-11", "2023-11-12", "two bathrooms", new_booking)
+
+
+@pytest.mark.restbooker
+def test_booking_getting(restbooker):
+    new_booking = restbooker.create_booking("Lester", "Tester", 12000, True, "2023-11-11", "2023-11-12", "two bathrooms")
+    get_booking = restbooker.get_booking(new_booking["bookingid"], 200)
+
+    restbooker.get_booking_assertion("Lester", "Tester", 12000, True, "2023-11-11", "2023-11-12", "two bathrooms", get_booking)
+
+
+@pytest.mark.restbooker
+def test_booking_updating(restbooker):
+    token = restbooker.auth()
+    new_booking = restbooker.create_booking("Lester", "Tester", 12000, True, "2023-11-11", "2023-11-12", "two bathrooms")
+    update_booking = restbooker.update_booking(token, new_booking["bookingid"], 
+                                               "Toster", "Rester", 5000, False, "2023-11-12", "2023-11-13", "additional key")
+
+    restbooker.update_booking_assertion("Toster", "Rester", 5000, False, "2023-11-12", "2023-11-13", "additional key", update_booking)
+    
+    get_booking = restbooker.get_booking(new_booking["bookingid"], 200)
+    restbooker.get_booking_assertion("Toster", "Rester", 5000, False, "2023-11-12", "2023-11-13", "additional key", get_booking)
+
+
+@pytest.mark.restbooker
+def test_booking_deleting(restbooker):
+    token = restbooker.auth()
+    new_booking = restbooker.create_booking("Lester", "Tester", 12000, True, "2023-11-11", "2023-11-12", "two bathrooms")
+
+    restbooker.delete_booking(token, new_booking)
+    restbooker.delete_booking_assertion(new_booking, 404)
 
 
 @pytest.mark.restbooker
