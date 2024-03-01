ba49e53e9ffa5ce54e2fc77283627676f2c57b74
Pavlo Lekhitskyi <pashokman1@gmail.com>
2024-03-01 13:03:03 +0200

    broken path to database fix 2.0

--- modules/common/database.py
+++ modules/common/database.py
@@ -4,7 +4,7 @@ import sqlite3
 class Database():
 
     def __init__(self):
-        self.connection = sqlite3.connect(r'../Become-QA-Auto' + r'/become_qa_auto.db')
+        self.connection = sqlite3.connect(r'./become_qa_auto.db')
         self.cursor = self.connection.cursor()
 
 
